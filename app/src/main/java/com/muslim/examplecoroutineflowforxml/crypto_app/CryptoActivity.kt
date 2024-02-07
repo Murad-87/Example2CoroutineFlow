2e127c81425af38f7dbfa0d81f7ad30c1af1b951
Murad <engno_95@mail.ru>
2024-02-07 12:47:12 +0300

    работа с builder flow - использование MutableSharedFlow

--- app/src/main/java/com/muslim/examplecoroutineflowforxml/crypto_app/CryptoActivity.kt
+++ app/src/main/java/com/muslim/examplecoroutineflowforxml/crypto_app/CryptoActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.muslim.examplecoroutineflowforxml.databinding.ActivityCryptoBinding
 import kotlinx.coroutines.launch
 
@@ -27,6 +29,10 @@ class CryptoActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupRecyclerView()
         observeViewModel()
+        binding.buttonRefreshList.setOnClickListener {
+
+            viewModel.refreshList()
+        }
     }
 
     private fun setupRecyclerView() {
@@ -36,19 +42,21 @@ class CryptoActivity : AppCompatActivity() {
 
     private fun observeViewModel() {
         lifecycleScope.launch {
-            viewModel.state.collect {
-                when (it) {
-                    is State.Initial -> {
-                        binding.progressBarLoading.isVisible = false
-                    }
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.state.collect {
+                    when (it) {
+                        is State.Initial -> {
+                            binding.progressBarLoading.isVisible = false
+                        }
 
-                    is State.Loading -> {
-                        binding.progressBarLoading.isVisible = true
-                    }
+                        is State.Loading -> {
+                            binding.progressBarLoading.isVisible = true
+                        }
 
-                    is State.Content -> {
-                        binding.progressBarLoading.isVisible = false
-                        adapter.submitList(it.currencyList)
+                        is State.Content -> {
+                            binding.progressBarLoading.isVisible = false
+                            adapter.submitList(it.currencyList)
+                        }
                     }
                 }
             }
--- app/src/main/java/com/muslim/examplecoroutineflowforxml/example/Example3ColdFlow.kt
+++ app/src/main/java/com/muslim/examplecoroutineflowforxml/example/Example3ColdFlow.kt
@@ -7,36 +7,36 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
-val scope = CoroutineScope(Dispatchers.IO)
+//val scope = CoroutineScope(Dispatchers.IO)
 
-suspend fun main() {
-
-    val job1 = scope.launch {
-        val flow = getFlow()
-        flow.collect {
-            println(it)
-        }
-    }
-
-    delay(5000)
-
-    val job2 = scope.launch {
-        val flow = getFlow()
-        flow.collect {
-            println(it)
-        }
-    }
-
-    job1.join()
-    job2.join()
-
-}
-
-fun getFlow(): Flow<Int> = flow {
-
-    repeat(100) {
-        println("Emitted: $it")
-        emit(it)
-        delay(1000)
-    }
-}
+//suspend fun main() {
+//
+//    val job1 = scope.launch {
+//        val flow = getFlow()
+//        flow.collect {
+//            println(it)
+//        }
+//    }
+//
+//    delay(5000)
+//
+//    val job2 = scope.launch {
+//        val flow = getFlow()
+//        flow.collect {
+//            println(it)
+//        }
+//    }
+//
+//    job1.join()
+//    job2.join()
+//
+//}
+//
+//fun getFlow(): Flow<Int> = flow {
+//
+//    repeat(100) {
+//        println("Emitted: $it")
+//        emit(it)
+//        delay(1000)
+//    }
+//}
